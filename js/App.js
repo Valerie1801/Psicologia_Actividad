b6d793949bb80433270c4d41a11f004756ef4da5
Juan David Ruiz <judarues@gmail.com>
2020-09-24 15:32:43 -0500

    Apellidos

--- js/App.js
+++ js/App.js
@@ -1,7 +1,8 @@
 class UsersAdmin {
-    constructor(cedula, nombre, correo, cargo, tipoUsuario, password) {
+    constructor(cedula, nombre, apellido, correo, cargo, tipoUsuario, password) {
 
         this.nombre = nombre;
+        this.apellido = apellido;
         this.cedula = cedula;
         this.correo = correo;
         this.cargo = cargo;
@@ -30,7 +31,8 @@ class UsersAdmin {
 let administrador = new UsersAdmin();
 let usuario = [{
     cedula: "12345",
-    nombre: "Juan Ruiz",
+    nombre: "Juan",
+    apellido: "Ruiz",
     correo: "JuanRuiz@mail.com",
     cargo: "Sistemas",
     tipoUsuario: "admin",
@@ -38,7 +40,8 @@ let usuario = [{
 },
 {
     cedula: "67890",
-    nombre: "Valerie Olivares",
+    nombre: "Valerie",
+    apellido: "Olivares",
     correo: "ValerieOlivares@mail.com",
     cargo: "Sistemas",
     tipoUsuario: "admin",
@@ -54,13 +57,13 @@ function ValidarLogin() {
     let password = document.getElementById("password").value;
 
     let listaUsers = JSON.parse(localStorage.getItem("Usuario"));
-    listaUsers.forEach(function (userLogin) {       
+    listaUsers.forEach(function (userLogin) {
 
         if (userLogin.correo == usuariomail && userLogin.password == password) {
             console.log("El usuario es de Tipo " + userLogin.tipoUsuario);
             if (userLogin.tipoUsuario == 'admin') {
                 document.location.href = "registro_Per.html";
-                alert("Bienvenido Sr(a) "+userLogin.nombre );
+                alert("Bienvenido Sr(a) " + userLogin.nombre);
             } else if (userLogin.tipoUsuario == 'user') {
                 document.location.href = "registro_Pac.html";
             }
