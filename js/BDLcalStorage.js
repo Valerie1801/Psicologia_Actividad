076aeaf8ad681fb26d983f63d480f227ddad4a59
Juan David Ruiz <judarues@gmail.com>
2020-09-23 21:53:20 -0500

    BDlocalstorage

---
+++ js/BDLcalStorage.js
@@ -0,0 +1,21 @@
+export default class LocalStorage {
+    constructor(){
+      this.stogare = localStorage
+    }
+    save = (name, data) => {
+      const result = this.getLocalStorage(name)
+      result.push(data)
+      this.stogare.setItem(name, JSON.stringify(result))
+    }
+  
+    getLocalStorage = (name) => {
+      let data
+      if (this.stogare.getItem(name) === null) {
+        data = []
+      } else {
+        data = JSON.parse(this.stogare.getItem(name))
+      }
+      return data
+    }
+  }
+  
