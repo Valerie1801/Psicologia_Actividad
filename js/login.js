a465aff552161d881d324ff6b3de6abeafd9a430
Juan David Ruiz <judarues@gmail.com>
2020-09-23 19:14:09 -0500

    Asignar Login

--- js/login.js
+++ js/login.js
@@ -1,44 +1,44 @@
-function ValidarLogin()
-{
+function ValidarLogin() {
     let usuario = document.getElementById("usuario").value;
     let password = document.getElementById("password").value;
-     
-    //console.log(JSON.stringify(Usuarios()));
-    Usuarios().forEach(function(userLogin) {       
-        if (userLogin.usuario == usuario && userLogin.password == password ) {
-          console.log("El usuario es de Tipo "+userLogin.tipoUsuario );          
-          document.location.href="Principal_a.html";
-          
-          
-          
-        }
-      });
 
-    
+    //console.log(JSON.stringify(Usuarios()));
+    Usuarios().forEach(function (userLogin) {
+        if (userLogin.usuario == usuario && userLogin.password == password) {
+
+            console.log("El usuario es de Tipo " + userLogin.tipoUsuario);
+            if (userLogin.tipoUsuario == 'admin') {
+                document.location.href = "Principal_a.html";
+            } else if (userLogin.tipoUsuario == 'user') {
+                document.location.href = "Principal_p.html";
+            }
+        }
+    });
+
+
 }
 
-function Usuarios()
-{
-    const listaUsuarios = 
-    [
-        {
-            usuario: 'psicologo',
-            password: 'itsa1234',
-            tipoUsuario: 'admin'
-        },
-        {
-            usuario: 'secretaria',
-            password: 'itsa4321',
-            tipoUsuario: 'auxiliar'
-        },
-        {
-            usuario: 'paciente',
-            password: 'itsa5678',
-            tipoUsuario: 'user'
-        }
+function Usuarios() {
+    const listaUsuarios =
+        [
+            {
+                usuario: 'psicologo',
+                password: 'itsa1234',
+                tipoUsuario: 'admin'
+            },
+            {
+                usuario: 'secretaria',
+                password: 'itsa4321',
+                tipoUsuario: 'auxiliar'
+            },
+            {
+                usuario: 'paciente',
+                password: 'itsa5678',
+                tipoUsuario: 'user'
+            }
 
-    ]
- 
+        ]
+
     return listaUsuarios
 }
 
